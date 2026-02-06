3d63120a5576f3b39a26e44ddb68a43086da7380
Gabriel Dumbrille <gabrieldumbrille@Mac.attlocal.net>
2026-02-06 08:49:28 -0600

    using precomputed delta

--- utils.py
+++ utils.py
@@ -86,8 +86,8 @@ def generate_queries_and_filter(spreadsheet, date_window_days=0):
                 }
                 query_params_list.append(query_params)
             
-            start_minus_one = (start_date - timedelta(days=1)).strftime("%Y%m%d")
-            end_plus_one = (end_date + timedelta(days=1)).strftime("%Y%m%d")
+                start_minus_one = (start_date - one_day_delta).strftime("%Y%m%d")
+                end_plus_one = (end_date + one_day_delta).strftime("%Y%m%d")
             filter_conditions.append(f'(PatientID.contains("{mrn}") * StudyDate.isGreaterThan("{start_minus_one}") * StudyDate.isLessThan("{end_plus_one}"))')
         else:
             raise ValueError(f"Row must have either acc_col or both mrn_col and date_col with valid values")
